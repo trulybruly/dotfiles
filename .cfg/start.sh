3a4e5497f1768ef33a0f653c3e763ff028b3f16d
trulybruly <wyoksher@yandex.ru>
2021-12-01 01:39:28 +0300

    Add quick script

---
+++ .cfg/start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+cd ~
+git clone --bare https://github.com/trulybruly/dotfiles.git $HOME/.cfg
+
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+
+config checkout
+
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    mkdir -p .config-backup
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+
+config checkout
+config config --local status.showUntrackedFiles no
+
+source ~/.zshrc
